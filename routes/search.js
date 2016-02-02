ca6fd0da55ab0c23b67f151285676268a1603838
Jessica Tran Hur <jesshur824@gmail.com>
2016-02-02 13:52:13 -0800

    separate logics for user's books, logics for getting all users

--- routes/search.js
+++ routes/search.js
@@ -7,35 +7,47 @@ const mongoose = require( 'mongoose' );
 
 router.use(bodyParser.json());
 
+//GET all users
+router.get('/users', (req, res, next) => {
+  User.find({})
+      .lean()
+      .exec( (err, users) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).send(err[0]);
+        }
+        res.send(users);
+      });
+});
+//GET all books by a specific user
+router.get('/userBooks', (req, res, next) => {
+  PhysicalBook.find({user_id: req.headers.user})
+              .then( books => {
+                return Promise.all(
+                  books.map( book => {
+                    return Book.findOne({ _id: book.book_id})
+                               .lean()
+                  })
+                );
+              })
+              .then( (books) => {
+                res.send(books);
+              })
+              .catch(err => {
+                console.log(err);
+                res.status(500).send(err[0]);
+              });
+})
+
 //GET all books
 router.get('/', (req, res, next) => {
-  if (Object.keys(req.query).length === 0 ) {
-    Book.find({}).lean().exec( (err, books) => {
-      if(err) {
-        console.log(err);
-        return res.status(500).send(err[0]);
-      }
-      res.send(books);
-    });
-  }
-  else {
-    PhysicalBook.find({user_id: req.query.user})
-                .then( books => {
-                  return Promise.all(
-                    books.map( book => {
-                      return Book.findOne({ _id: book.book_id})
-                                 .lean()
-                    })
-                  );
-                })
-                .then( (books) => {
-                  res.send(books);
-                })
-                .catch(err => {
-                  console.log(err);
-                  res.status(500).send(err[0]);
-                });
-  }
+  Book.find({}).lean().exec( (err, books) => {
+    if(err) {
+      console.log(err);
+      return res.status(500).send(err[0]);
+    }
+    res.send(books);
+  });
 });
 
 //GET specific book
@@ -48,5 +60,6 @@ router.get('/:book_id', (req, res, next) => {
     res.send(book);
   })
 });
+
 
 module.exports = router;
