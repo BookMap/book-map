44560844a3e78ec7adf65523dc8af638d965e74c
Jessica Tran Hur <jesshur824@gmail.com>
2016-02-03 15:04:21 -0800

    remove comment

--- public/components/public-profile/public-profile.js
+++ public/components/public-profile/public-profile.js
@@ -1,7 +1,6 @@
 angular.module( 'controllers' ).controller('PublicProfileCtrl', ['$scope','$http', '$window',
   function ($scope, $http, $window) {
     var temp = JSON.parse($window.localStorage.temp);
-    // delete $window.localStorage.temp;
     $scope.picture = 'https://graph.facebook.com/' + temp.id + '/picture?height=200&width=200';
     $http.get(`/api/search?search=books&userId=${temp.id}`)
          .then( function(res) {
