e3947ec9f6802ecaf4a1d3377ead5c46b7f12f28
Jessica Tran Hur <jesshur824@gmail.com>
2016-02-03 14:35:36 -0800

    got user ID without exposing it on the url

--- public/components/public-profile/public-profile.js
+++ public/components/public-profile/public-profile.js
@@ -1,5 +1,11 @@
 angular.module( 'controllers' ).controller('PublicProfileCtrl', ['$scope','$http', '$window',
   function ($scope, $http, $window) {
-    console.log($window.localtion);
+    var url = $window.location.hash.split('/');
+    var userId = url[url.length-1];
+    $http.get(`/api/search?search=books&userId=${$window.localStorage.temp}`)
+         .then( function(res) {
+            delete $window.localStorage.temp;
+            console.log(res);
+         })
   }
 ]);
--- public/components/user-list/user-list.js
+++ public/components/user-list/user-list.js
@@ -1,7 +1,10 @@
-angular.module( 'controllers' ).controller( 'UserListCtrl', [ '$scope', '$http',
-    function( $scope, $http ) {
+angular.module( 'controllers' ).controller( 'UserListCtrl', [ '$scope', '$http', '$window',
+    function( $scope, $http, $window ) {
         $http.get( '/api/search?search=users' ).then( function( res ) {
           $scope.users = res.data;
         });
+        $scope.getUser = function (user){
+          $window.localStorage.temp = user._id;
+        }
     }]
 );
